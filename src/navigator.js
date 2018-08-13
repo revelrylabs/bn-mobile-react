91e68f9ee18e6255492a56389a7c04f3ec01d10f
brittanygay <brittany@revelry.co>
2018-08-13 14:58:00 -0500

    Added imgs, updated navigation settings

--- src/navigator.js
+++ src/navigator.js
@@ -1,7 +1,10 @@
 import { createStackNavigator, createBottomTabNavigator, BottomTabBar } from 'react-navigation'
+import { Image } from 'react-native';
 import EventRoutes from './Events/routes'
 import TicketRoutes from './Tickets/routes'
 import AccountRoutes from './Accounts/routes'
+
+const TabBarComponent = (props) => (<BottomTabBar {...props} />);
 
 const EventsStack = createStackNavigator({
   ...EventRoutes,
@@ -21,11 +24,46 @@ const AccountsStack =  createStackNavigator({
   initialRouteName: 'Account',
 })
 
+const tabBarOptions = {
+  tabBarOptions: {
+    activeTintColor: '#FF20B1',
+    inactiveTintColor: 'black',
+    labelStyle: {
+      fontFamily: 'tt_commons_demibold',
+      fontSize: 14,
+      paddingVertical: 10,
+    },
+    style: {
+      backgroundColor: 'white',
+      borderWidth: 0,
+      borderTopColor: 'white',
+    },
+  }
+}
 
 export default createBottomTabNavigator(
   {
-    Explore: EventsStack,
-    MyTickets: TicketsStack,
-    Account: AccountsStack,
-  }
+    Explore: {
+      screen: EventsStack,
+      navigationOptions: {
+        tabBarIcon: <Image source={require('../assets/heart-small.png')}/>,
+        showIcon: true,
+      }
+    },
+    MyTickets: {
+      screen: TicketsStack,
+      navigationOptions: {
+        tabBarIcon: <Image source={require('../assets/heart-small.png')}/>,
+        showIcon: true,
+      }
+    },
+    Account: {
+      screen: AccountsStack,
+      navigationOptions: {
+        tabBarIcon: <Image source={require('../assets/heart-small.png')}/>,
+        showIcon: true,
+      }
+    },
+  },
+  {tabBarOptions}
 )
