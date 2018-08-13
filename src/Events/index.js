9b353c2356481b60a109d18c1bd7e7396a338a2c
jwietelmann <jwietelmann@gmail.com>
2018-08-13 11:22:29 -0400

    DRY up the event cards

--- src/Events/index.js
+++ src/Events/index.js
@@ -11,8 +11,40 @@ const formStyles = FormStyles.createStyles()
 const slideshowStyles = SlideShowStyles.createStyles()
 const eventStyles = EventStyles.createStyles()
 
+function EventItemView({ bgImage, avatarImages, priceDollars, titleText, scheduleText }) {
+  return (
+    <View style={eventStyles.eventContainer}>
+      <Image
+        style={eventStyles.eventImage}
+        source={bgImage}
+      />
+
+      <View style={eventStyles.detailsContainer}>
+        <View style={eventStyles.sectionTop}>
+          <View style={eventStyles.iconLinkStarContainerSmall}>
+            <Icon style={eventStyles.iconLinkStarSmall} name="star" />
+          </View>
+          <View style={styles.avatarContainer}>
+            {avatarImages.map(source => <Image style={styles.avatarSmall} source={source} />)}
+          </View>
+        </View>
+        <View style={eventStyles.sectionBottom}>
+          <View style={styles.priceTagContainer}>
+            <Text style={styles.priceTag}>${priceDollars}</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={eventStyles.detailsContainerBottom}>
+        <Text style={eventStyles.header}>{titleText}</Text>
+        <Text style={eventStyles.details}>{scheduleText}</Text>
+      </View>
+    </View>
+  )
+}
+
 export default function EventsIndex(props) {
-  const {navigation: {navigate}} = props
+  const { navigation: { navigate } } = props
 
   return (
     <ScrollView style={styles.container}>
@@ -75,169 +107,52 @@ export default function EventsIndex(props) {
         </View>
       </View>
 
-
-
       <Text style={styles.sectionHeader}>Upcoming</Text>
 
-      <View style={eventStyles.eventContainer}>
-        <Image
-          style={eventStyles.eventImage}
-          source={require('../../assets/event-smaller-1.png')}
-        />
-
-        <View style={eventStyles.detailsContainer}>
-          <View style={eventStyles.sectionTop}>
-            <View style={eventStyles.iconLinkStarContainerSmall}>
-              <Icon style={eventStyles.iconLinkStarSmall} name="star" />
-            </View>
-            <View style={styles.avatarContainer}>
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-male.png')}
-              />
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-female.png')}
-              />
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-female.png')}
-              />
-            </View>
-          </View>
-          <View style={eventStyles.sectionBottom}>
-            <View style={styles.priceTagContainer}>
-              <Text style={styles.priceTag}>$30</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={eventStyles.detailsContainerBottom}>
-          <Text style={eventStyles.header}>River Whyless</Text>
-          <Text style={eventStyles.details}>Fri, July 20 - 8:50 pm - The Warfield</Text>
-        </View>
-      </View>
-
-
-      <View style={eventStyles.eventContainer}>
-        <Image
-          style={eventStyles.eventImage}
-          source={require('../../assets/event-smaller-2.png')}
-        />
-
-        <View style={eventStyles.detailsContainer}>
-          <View style={eventStyles.sectionTop}>
-            <View style={eventStyles.iconLinkStarContainerSmall}>
-              <Icon style={eventStyles.iconLinkStarSmall} name="star" />
-            </View>
-            <View style={styles.avatarContainer}>
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-female.png')}
-              />
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-male.png')}
-              />
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-female.png')}
-              />
-            </View>
-          </View>
-          <View style={eventStyles.sectionBottom}>
-            <View style={styles.priceTagContainer}>
-              <Text style={styles.priceTag}>$30</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={eventStyles.detailsContainerBottom}>
-          <Text style={eventStyles.header}>Beyonce</Text>
-          <Text style={eventStyles.details}>Fri, July 20 - 8:50 pm - The Warfield</Text>
-        </View>
-      </View>
-
-
-      <View style={eventStyles.eventContainer}>
-        <Image
-          style={eventStyles.eventImage}
-          source={require('../../assets/event-smaller-3.png')}
-        />
-
-        <View style={eventStyles.detailsContainer}>
-          <View style={eventStyles.sectionTop}>
-            <View style={eventStyles.iconLinkStarContainerSmall}>
-              <Icon style={eventStyles.iconLinkStarSmall} name="star" />
-            </View>
-            <View style={styles.avatarContainer}>
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-male.png')}
-              />
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-female.png')}
-              />
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-female.png')}
-              />
-            </View>
-          </View>
-          <View style={eventStyles.sectionBottom}>
-            <View style={styles.priceTagContainer}>
-              <Text style={styles.priceTag}>$30</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={eventStyles.detailsContainerBottom}>
-          <Text style={eventStyles.header}>Drake</Text>
-          <Text style={eventStyles.details}>Fri, July 20 - 8:50 pm - The Warfield</Text>
-        </View>
-      </View>
-
-
-      <View style={eventStyles.eventContainer}>
-        <Image
-          style={eventStyles.eventImage}
-          source={require('../../assets/event-smaller-4.png')}
-        />
-
-        <View style={eventStyles.detailsContainer}>
-          <View style={eventStyles.sectionTop}>
-            <View style={eventStyles.iconLinkStarContainerSmall}>
-              <Icon style={eventStyles.iconLinkStarSmall} name="star" />
-            </View>
-            <View style={styles.avatarContainer}>
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-male.png')}
-              />
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-female.png')}
-              />
-              <Image
-                style={styles.avatarSmall}
-                source={require('../../assets/avatar-female.png')}
-              />
-            </View>
-          </View>
-          <View style={eventStyles.sectionBottom}>
-            <View style={styles.priceTagContainer}>
-              <Text style={styles.priceTag}>$30</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={eventStyles.detailsContainerBottom}>
-          <Text style={eventStyles.header}>Ed Sheeran</Text>
-          <Text style={eventStyles.details}>Fri, July 20 - 8:50 pm - The Warfield</Text>
-        </View>
-      </View>
-
+      <EventItemView
+        bgImage={require('../../assets/event-smaller-1.png')}
+        avatarImages={[
+          require('../../assets/avatar-male.png'),
+          require('../../assets/avatar-female.png'),
+          require('../../assets/avatar-female.png'),
+        ]}
+        priceDollars={30}
+        titleText="River Whyless"
+        scheduleText="Fri, July 20 - 8:50 pm - The Warfield"
+      />
+      <EventItemView
+        bgImage={require('../../assets/event-smaller-2.png')}
+        avatarImages={[
+          require('../../assets/avatar-female.png'),
+          require('../../assets/avatar-male.png'),
+          require('../../assets/avatar-female.png'),
+        ]}
+        priceDollars={30}
+        titleText="Beyonce"
+        scheduleText="Fri, July 20 - 8:50 pm - The Warfield"
+      />
+      <EventItemView
+        bgImage={require('../../assets/event-smaller-3.png')}
+        avatarImages={[
+          require('../../assets/avatar-male.png'),
+          require('../../assets/avatar-female.png'),
+          require('../../assets/avatar-female.png'),
+        ]}
+        priceDollars={30}
+        titleText="Drake"
+        scheduleText="Fri, July 20 - 8:50 pm - The Warfield"
+      />
+      <EventItemView
+        bgImage={require('../../assets/event-smaller-4.png')}
+        avatarImages={[
+          require('../../assets/avatar-male.png'),
+          require('../../assets/avatar-female.png'),
+          require('../../assets/avatar-female.png'),
+        ]}
+        priceDollars={30}
+        titleText="Ed Sheeran"
+        scheduleText="Fri, July 20 - 8:50 pm - The Warfield"
+      />
     </ScrollView>
   );
 }
