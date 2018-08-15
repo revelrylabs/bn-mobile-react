70e06bbd5b149a087758e6a092d1009360b66208
brittanygay <brittany@revelry.co>
2018-08-15 13:34:30 -0500

    page setup

--- src/Events/show.js
+++ src/Events/show.js
@@ -1,13 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, Image, TouchableHighlight } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import SharedStyles from '../styles/shared/sharedStyles'
+import SlideShowStyles from '../styles/shared/slideshowStyles'
+import EventDetailsStyles from '../styles/shared/eventDetailsStyles'
 
 const styles = SharedStyles.createStyles()
+const slideshowStyles = SlideShowStyles.createStyles()
+const eventDetailsStyles = EventDetailsStyles.createStyles()
 
 export default function EventsShow() {
   return (
-    <View style={styles.container}>
-      <Text>Show a specific Event</Text>
-    </View>
+    <ScrollView style={styles.container}>
+
+      <View style={slideshowStyles.slideshowContainer}>
+        <Image
+          style={slideshowStyles.slideShowImage}
+          source={require('../../assets/featured-1.png')}
+        />
+
+        <View style={slideshowStyles.detailsContainer}>
+          <View style={slideshowStyles.sectionTop}>
+            <View style={styles.iconLinkStarContainer}>
+              <Icon style={styles.iconLinkStar} name="star" />
+            </View>
+            <View style={styles.avatarContainer}>
+              <Image
+                style={styles.avatar}
+                source={require('../../assets/avatar-male.png')}
+              />
+              <Image
+                style={styles.avatar}
+                source={require('../../assets/avatar-female.png')}
+              />
+            </View>
+          </View>
+
+          <View style={slideshowStyles.sectionMiddle}>
+            <Icon style={slideshowStyles.slideShowIconLinkLeft} name="keyboard-arrow-left" />
+            <Icon style={slideshowStyles.slideShowIconLinkRight} name="keyboard-arrow-right" />
+          </View>
+
+          <TouchableHighlight underlayColor="rgba(0, 0, 0, 0)" onPress={() => navigate('EventsShow', {name: 'Childish Gambino'})}>
+            <View style={slideshowStyles.sectionBottom}>
+              <View style={styles.priceTagContainer}>
+                <Text style={styles.priceTag}>$30</Text>
+              </View>
+              <Text style={slideshowStyles.header}>Childish Gambino</Text>
+              <Text style={slideshowStyles.details}>Fri, July 20 - 8:50 pm - The Warfield</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+      </View>
+
+    </ScrollView>
   );
 }
