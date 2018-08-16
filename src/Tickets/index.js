5c2509844171d39229256570c67dfebf14e68a9f
brittanygay <brittany@revelry.co>
2018-08-16 15:41:06 -0500

    page setup

--- src/Tickets/index.js
+++ src/Tickets/index.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, Button, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, Button, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import SharedStyles from '../styles/shared/sharedStyles'
+import FormStyles from '../styles/shared/formStyles'
+import SlideShowStyles from '../styles/shared/slideshowStyles'
+import EventStyles from '../styles/shared/eventStyles'
 
 const styles = SharedStyles.createStyles()
+const formStyles = FormStyles.createStyles()
+const slideshowStyles = SlideShowStyles.createStyles()
+const eventStyles = EventStyles.createStyles()
+
 
 export default function MyTickets(props) {
   const {navigation: {navigate}} = props
 
   return (
-    <View style={styles.container}>
-      <Text>My Tickets Page</Text>
+    <ScrollView style={styles.container}>
+
+      <View style={styles.sectionHeaderContainer}>
+        <Text style={styles.header}>Explore</Text>
+        <View style={styles.iconLinkContainer}>
+          <Image
+            style={styles.iconImageSmall}
+            source={require('../../assets/heart-small.png')}
+          />
+          <Text style={styles.iconLinkText}>NYC</Text>
+          <Icon style={styles.iconLink} name="keyboard-arrow-down" />
+        </View>
+      </View>
+
+
+
       <Button
         title="View Event Ticket"
         onPress={() => navigate("EventTicket")}
       />
-    </View>
+    </ScrollView>
   );
 }
