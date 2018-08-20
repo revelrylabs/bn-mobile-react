cf702e8a02f955f080411be54dacbbb8b1c83b5d
brittanygay <brittany@revelry.co>
2018-08-20 13:58:38 -0500

    Added new stylesheet

--- src/Tickets/show.js
+++ src/Tickets/show.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import SharedStyles from '../styles/shared/sharedStyles'
+import TicketShowStyles from '../styles/tickets/ticketShowStyles'
 
 const styles = SharedStyles.createStyles()
+const ticketShowStyles = TicketShowStyles.createStyles()
 
 export default function EventsTicket() {
   return (
     <View style={styles.container}>
-      <Text>Show a specific Event&apos;s Ticket</Text>
+      <Icon
+        style={styles.iconLinkCircle}
+        name="close"
+        onPress={() => {
+          this.props.navigation.goBack()
+        }}
+      />
     </View>
   );
 }
