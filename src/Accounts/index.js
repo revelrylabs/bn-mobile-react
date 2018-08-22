03d2b7fa77e9d6555e7678cc912be8a22cd747f1
brittanygay <brittany@revelry.co>
2018-08-22 10:50:51 -0500

    added stylesheet

--- src/Accounts/index.js
+++ src/Accounts/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SharedStyles from '../styles/shared/sharedStyles'
+import AccountStyles from '../styles/account/accountStyles'
 
 const styles = SharedStyles.createStyles()
+const accountStyles = AccountStyles.createStyles()
 
 export default function Account() {
   return (
     <View style={styles.container}>
-      <Text>My Account Details</Text>
     </View>
   );
 }
