66f7bc431fe1017a403b78e2420b4ec53371cba0
brittanygay <brittany@revelry.co>
2018-09-04 11:57:55 -0500

    added new stylesheet

--- src/Accounts/notifications.js
+++ src/Accounts/notifications.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import {Text, View, ScrollView} from 'react-native';
+import {Text, View, ScrollView, TouchableHighlight} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import SharedStyles from '../styles/shared/sharedStyles'
 import AccountStyles from '../styles/account/accountStyles'
+import NotificationStyles from '../styles/account/notificationStyles'
 
 const styles = SharedStyles.createStyles()
 const accountStyles = AccountStyles.createStyles()
+const notificationStyles = NotificationStyles.createStyles()
 
 export default function Notifications() {
   return (
     <ScrollView>
       <View style={accountStyles.containerDark}>
+
         <Text style={accountStyles.sectionHeader}>Notification Type</Text>
+
+        <TouchableHighlight underlayColor="rgba(0, 0, 0, 0)" onPress={() => navigate('AccountDetails')}>
+          <View style={accountStyles.accountRow}>
+            <View style={accountStyles.accountRowWrapper}>
+              <Icon style={accountStyles.accountIcon} name="account-circle" />
+              <Text style={accountStyles.accountHeader}>Account</Text>
+            </View>
+            <Icon style={accountStyles.accountArrow} name="keyboard-arrow-right" />
+          </View>
+        </TouchableHighlight>
+
       </View>
     </ScrollView>
   );
---
+++ src/styles/account/notificationStyles.js
@@ -0,0 +1,47 @@
+import {
+  textColor,
+  sectionHeaderColor,
+  white,
+  borderColor,
+  containerDarkColor,
+  disabledHeaderColor,
+  globalFontRegular,
+  globalFontMedium,
+  globalPaddingLarge,
+  globalPaddingMedium,
+  globalPadding,
+  globalPaddingSmall,
+  globalPaddingTiny,
+  globalMarginSmall,
+} from '../shared/sharedStyles'
+import {StyleSheet, Dimensions, Platform} from 'react-native'
+const fullHeight = Dimensions.get('window').height
+const fullWidth = Dimensions.get('window').width
+
+export const whiteTransparent = 'rgba(255, 255, 255, 0.8)'
+
+export const headerFontSize = 28
+export const sectionHeaderFontSize = 24
+export const bodyFontSize = 16
+export const bodyFontSizeSmall = 14
+export const iconFontSize = 18
+export const avatarIconFontSize = 28
+export const iconLargeFontSize = 56
+
+const NotificationStyles = {
+  accountBkgdContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 240,
+  },
+}
+
+function createStyles(overrides = {}) {
+  return StyleSheet.create({...NotificationStyles, ...overrides})
+}
+
+export default {
+  createStyles,
+}
