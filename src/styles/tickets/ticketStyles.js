2a77bc87dfaae6e44cf9dc60716567f7a42856af
brittanygay <brittany@revelry.co>
2018-09-10 14:45:59 -0500

    ticket stylesheet cleanup

--- src/styles/tickets/ticketStyles.js
+++ src/styles/tickets/ticketStyles.js
@@ -1,48 +1,25 @@
+import {
+  white,
+  sectionHeaderColor,
+  globalFontRegular,
+  globalPadding,
+  globalPaddingSmall,
+  globalPaddingTiny,
+  globalMargin,
+} from '../shared/sharedStyles'
 import {StyleSheet, Dimensions, Platform} from 'react-native'
 const fullHeight = Dimensions.get('window').height
 const fullWidth = Dimensions.get('window').width
 
-export const primaryColor = '#FF20B1'
-export const secondaryColor = '#707CED'
-export const tertiaryColor = '#4EB0E5'
-export const white = '#FFF'
-export const errorTextColor = '#FF0000'
-export const textColor = '#1E1E1E'
-export const backgroundColor = white
-export const inputBackgroundColor = '#FAFAFA'
-export const disabledColor = '#F7F7F7'
-export const helpTextColor = '#666'
-export const borderColor = '#DCDCDC'
-export const placeholderImageBackground = '#F6F7F5'
-export const sectionHeaderColor = '#9DA3B4'
-export const primaryTransparent = 'rgba(255, 34, 178, 0.5)'
 export const whiteTransparent = 'rgba(255, 255, 255, 0.8)'
 
-export const globalPaddingTiny = 5
-export const globalPaddingSmall = 10
-export const globalPadding = 20
-export const globalPaddingMedium = 30
-export const globalPaddingLarge = 40
-export const globalPaddingLarger = 45
-export const globalPaddingJumbo = 55
-export const globalMarginSmall = 10
-export const globalMargin = 20
-
-export const globalFontRegular = 'tt_commons_regular'
-export const globalFontMedium = 'tt_commons_medium'
-export const globalFontSemiBold = 'tt_commons_demibold'
-export const globalFontBold = 'tt_commons_bold'
-export const globalFontItalic = 'tt_commons_italic'
-
 export const headerFontSize = 28
-export const sectionHeaderFontSize = 18
 export const bodyFontSize = 16
 export const bodyFontSizeSmall = 13
 export const iconFontSize = 18
-export const iconLargeFontSize = 56
-export const slideShowArrowFontSize = 28
 
 const TicketStyles = {
+  // CONTAINER STYLES
   ticketContainer: {
     marginTop: globalMargin,
     padding: globalPadding,
@@ -51,12 +28,6 @@ const TicketStyles = {
     flexDirection: 'column',
     justifyContent: 'space-between',
     height: 145,
-  },
-  header: {
-    fontFamily: globalFontRegular,
-    fontSize: headerFontSize,
-    color: white,
-    marginBottom: globalPaddingTiny,
   },
   ticketContainerBottom: {
     backgroundColor: white,
@@ -68,13 +39,13 @@ const TicketStyles = {
     justifyContent: 'space-between',
     padding: globalPaddingSmall + globalPaddingTiny,
   },
-  iconTicket: {
+
+  // TEXT STYLES
+  header: {
+    fontFamily: globalFontRegular,
+    fontSize: headerFontSize,
     color: white,
-    fontSize: iconFontSize,
-    marginRight: globalPaddingTiny,
-  },
-  iconTicketText: {
-    color: white,
+    marginBottom: globalPaddingTiny,
   },
   detailsBottomHeader: {
     color: sectionHeaderColor,
@@ -88,7 +59,17 @@ const TicketStyles = {
   },
   detailsLast: {
     textAlign: 'right',
-  }
+  },
+
+  // ICON STYLES
+  iconTicket: {
+    color: white,
+    fontSize: iconFontSize,
+    marginRight: globalPaddingTiny,
+  },
+  iconTicketText: {
+    color: white,
+  },
 }
 
 function createStyles(overrides = {}) {
