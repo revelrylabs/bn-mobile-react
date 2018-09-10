e536584790d1a6dcd85cf817b6f66d9546816eee
brittanygay <brittany@revelry.co>
2018-09-10 13:54:58 -0500

    event stylesheet cleanup

--- src/styles/shared/eventStyles.js
+++ src/styles/shared/eventStyles.js
@@ -1,36 +1,17 @@
+import {
+  white,
+  textColor,
+  globalFontRegular,
+  globalFontSemiBold,
+  globalPadding,
+  globalPaddingSmall,
+  globalPaddingTiny,
+} from '../shared/sharedStyles'
 import {StyleSheet, Dimensions, Platform} from 'react-native'
 const fullHeight = Dimensions.get('window').height
 const fullWidth = Dimensions.get('window').width
 
-export const primaryColor = '#FF20B1'
-export const secondaryColor = '#707CED'
-export const tertiaryColor = '#4EB0E5'
-export const white = '#FFF'
-export const errorTextColor = '#FF0000'
-export const textColor = '#1E1E1E'
-export const backgroundColor = white
-export const inputBackgroundColor = '#FAFAFA'
-export const disabledColor = '#F7F7F7'
-export const helpTextColor = '#666'
-export const borderColor = '#DCDCDC'
-export const primaryTransparent = 'rgba(255, 34, 178, 0.5)'
 export const whiteTransparent = 'rgba(255, 255, 255, 0.10)'
-
-export const globalPaddingTiny = 5
-export const globalPaddingSmall = 10
-export const globalPadding = 20
-export const globalPaddingMedium = 30
-export const globalPaddingLarge = 40
-export const globalPaddingLarger = 45
-export const globalPaddingJumbo = 55
-export const globalMarginSmall = 10
-export const globalMargin = 20
-
-export const globalFontRegular = 'tt_commons_regular'
-export const globalFontMedium = 'tt_commons_medium'
-export const globalFontSemiBold = 'tt_commons_demibold'
-export const globalFontBold = 'tt_commons_bold'
-export const globalFontItalic = 'tt_commons_italic'
 
 export const headerFontSize = 38
 export const sectionHeaderFontSize = 21
@@ -38,6 +19,7 @@ export const bodyFontSize = 16
 export const iconFontSize = 18
 
 const EventStyles = {
+  // CONTAINER STYLES
   eventContainer: {
     padding: globalPaddingSmall,
   },
@@ -56,6 +38,20 @@ const EventStyles = {
   sectionBottom: {
     paddingVertical: globalPadding,
   },
+
+  // TEXT STYLES
+  header: {
+    fontFamily: globalFontSemiBold,
+    fontSize: sectionHeaderFontSize,
+    color: textColor,
+  },
+  details: {
+    fontFamily: globalFontRegular,
+    fontSize: bodyFontSize,
+    color: textColor,
+  },
+
+  // ICON STYLES
   iconLinkCircleContainerSmall: {
     backgroundColor: whiteTransparent,
     borderRadius: 100/2,
@@ -67,20 +63,12 @@ const EventStyles = {
     color: white,
     fontSize: iconFontSize,
   },
+
+  // IMAGE BKGD STYLES
   eventImage: {
     height: 180,
     position: 'absolute',
     width: fullWidth - 43,
-  },
-  header: {
-    fontFamily: globalFontSemiBold,
-    fontSize: sectionHeaderFontSize,
-    color: textColor,
-  },
-  details: {
-    fontFamily: globalFontRegular,
-    fontSize: bodyFontSize,
-    color: textColor,
   },
 }
 
