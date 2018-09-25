346bd6fe1a1eeacc44b9a95786c589ba5ca05bcb
brittanygay <brittany@revelry.co>
2018-09-25 13:33:55 -0500

    moved styles to new stylesheet

--- src/Events/tickets.js
+++ src/Events/tickets.js
@@ -2,8 +2,12 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Text, View, TouchableHighlight, ScrollView} from 'react-native'
 import SharedStyles from '../styles/shared/sharedStyles'
+import AccountStyles from '../styles/account/accountStyles'
+import EventTicketStyles from '../styles/shared/eventTicketStyles'
 
 const styles = SharedStyles.createStyles()
+const accountStyles = AccountStyles.createStyles()
+const eventTicketStyles = EventTicketStyles.createStyles()
 
 export default class GetTickets extends Component {
   static propTypes = {
@@ -14,10 +18,22 @@ export default class GetTickets extends Component {
     return (
       <ScrollView>
         <View style={styles.container}>
+
           <View style={styles.sectionHeaderContainer}>
             <Text style={styles.header}>Ticket Type</Text>
           </View>
-        </View>  
+
+          <TouchableHighlight>
+            <View style={eventTicketStyles.rowContainer}>
+              <View style={eventTicketStyles.row}>
+                <Icon style={eventTicketStyles.ticketIcon} name="account-circle" />
+                <Text style={eventTicketStyles.ticketHeader}>Account</Text>
+              </View>
+              <Icon style={accountStyles.accountArrow} name="keyboard-arrow-right" />
+            </View>
+          </TouchableHighlight>
+
+        </View>
       </ScrollView>
     )
   }
---
+++ src/styles/event_details/eventTicketStyles.js
@@ -0,0 +1,48 @@
+import {
+  primaryColor,
+} from '../shared/sharedStyles'
+import {StyleSheet, Dimensions, Platform} from 'react-native'
+const fullHeight = Dimensions.get('window').height
+const fullWidth = Dimensions.get('window').width
+
+export const whiteTransparent = 'rgba(255, 255, 255, 0.8)'
+
+
+const EventTicketStyles = {
+  // ROW STYLES
+  rowContainer: {
+    alignItems: 'center',
+    backgroundColor: white,
+    borderBottomColor: borderColor,
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: globalPadding,
+    paddingVertical: globalPaddingSmall,
+    width: fullWidth,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+
+  // ACCOUNT SUBNAV STYLES
+  ticketIcon: {
+    color: sectionHeaderColor,
+    fontSize: bodyFontSize,
+    paddingRight: globalPaddingSmall,
+  },
+  ticketHeader: {
+    color: textColor,
+    fontFamily: globalFontRegular,
+    fontSize: iconFontSize,
+  },
+}
+
+function createStyles(overrides = {}) {
+  return StyleSheet.create({...EventTicketStyles, ...overrides})
+}
+
+export default {
+  createStyles,
+}
