2d1fcbb21db0bb683201413d1e80a9a40c19d8ef
brittanygay <brittany@revelry.co>
2018-09-25 16:19:42 -0500

    screen stylesheet setup

--- src/Events/payments.js
+++ src/Events/payments.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {Text, View, TouchableHighlight} from 'react-native'
+import {Text, View, TouchableHighlight, ScrollView} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import SharedStyles from '../styles/shared/sharedStyles'
+import AccountStyles from '../styles/account/accountStyles'
+import EventTicketStyles from '../styles/event_details/eventTicketStyles'
+
+const styles = SharedStyles.createStyles()
+const accountStyles = AccountStyles.createStyles()
+const eventTicketStyles = EventTicketStyles.createStyles()
 
 export default class PaymentTypes extends Component {
   static propTypes = {
