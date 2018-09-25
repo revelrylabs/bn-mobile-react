162d17f68ab5c56c91be7434f9bd2f3d1e963d00
brittanygay <brittany@revelry.co>
2018-09-25 11:44:57 -0500

    page setup

--- src/Events/tickets.js
+++ src/Events/tickets.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {Text, View, TouchableHighlight} from 'react-native'
+import {Text, View, TouchableHighlight, ScrollView} from 'react-native'
+import SharedStyles from '../styles/shared/sharedStyles'
+
+const styles = SharedStyles.createStyles()
 
 export default class GetTickets extends Component {
   static propTypes = {
@@ -9,9 +12,13 @@ export default class GetTickets extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Tickets</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.sectionHeaderContainer}>
+            <Text style={styles.header}>Ticket Type</Text>
+          </View>
+        </View>  
+      </ScrollView>
     )
   }
 }
