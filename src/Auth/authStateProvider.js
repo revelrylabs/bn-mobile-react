efd31120956012048ec92795fa602d5fc3d33928
Brad Huber <daybreaker@gmail.com>
2018-10-17 12:04:32 -0500

    stash - working login

--- src/Auth/authStateProvider.js
+++ src/Auth/authStateProvider.js
@@ -1,4 +1,5 @@
 import {Container} from 'unstated'
+import {AsyncStorage} from 'react-native'
 import Bigneon from 'bn-api-node'
 
 const server = new Bigneon.Server({prefix: 'https://staging.bigneon.com/api'});
@@ -9,6 +10,8 @@ class AuthContainer extends Container {
 
     this.state = {
       currentUser: {},
+      access_token: null,
+      refresh_tokn: null,
     };
   }
 
@@ -17,20 +20,35 @@ class AuthContainer extends Container {
   }
 
   // @TODO: Implement a login that also sets AsyncStgorage user
-  // logIn = () => {
+  logIn = async (formData, navigate) => {
+    try {
+      const resp = await server.auth.authenticate(formData)
+      const {data: {access_token, refresh_token}} = resp
 
-  // }
+      await AsyncStorage.setItem('userToken', access_token)
+      await AsyncStorage.setItem('refreshToken', refresh_token)
 
-  signUp = (formData, success) => {
+      server.users.current().then((myUserResponse) => {
+        this.setState({currentUser: myUserResponse.data})
+      });
+
+    } catch {
+      navigate('LogIn')
+    }
+
+    navigate('AuthLoading')
+  }
+
+  signUp = (formData, navigate) => {
     server.users.register({
-      first_name: "Bob",
-      last_name: "Smith",
+      first_name: 'Bob',
+      last_name: 'Smith',
       email: formData.email,
       password: formData.password,
-      phone: "5551234567"
+      phone: '5551234567',
     }).then((res) => {
       console.log(res);
-      success // should redirect to LogIn with a message
+      navigate('LogIn') // should redirect to LogIn with a message
     }).catch((e) => {
       console.error(e);
     });
