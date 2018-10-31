986c6e95c22743479d9de1a0ad2790d78cb8bc73
brittanygay <brittany@revelry.co>
2018-10-31 12:31:55 -0500

    modal container styles

--- src/Tickets/transferTickets.js
+++ src/Tickets/transferTickets.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types'
-import {Modal, ScrollView, Text, View, Image} from 'react-native';
+import {Modal, ScrollView, Text, View, Image, TouchableHighlight} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import SharedStyles from '../styles/shared/sharedStyles'
 import TicketWalletStyles from '../styles/tickets/ticketWalletStyles'
+import ModalStyles from '../styles/shared/modalStyles'
 
 const styles = SharedStyles.createStyles()
 const ticketWalletStyles = TicketWalletStyles.createStyles()
+const modalStyles = ModalStyles.createStyles()
 
 export default class TransferTickets extends Component {
 
@@ -19,7 +21,6 @@ export default class TransferTickets extends Component {
             style={ticketWalletStyles.modalBkgdImage}
             source={require('../../assets/modal-bkgd.jpg')}
           />
-
           <View>
             <View style={ticketWalletStyles.closeModalContainer}>
               <Icon
@@ -30,6 +31,30 @@ export default class TransferTickets extends Component {
                 }}
               />
             </View>
+          </View>
+
+          <View style={modalStyles.contentWrapper}>
+
+            <View style={ticketWalletStyles.qrCodeContainer}>
+              <Image
+                style={ticketWalletStyles.qrCode}
+                source={require('../../assets/qr-code-placeholder.png')}
+              />
+            </View>
+
+            <View style={ticketWalletStyles.bottomNav}>
+              <View style={[ticketWalletStyles.bottomNavLinkContainer, styles.borderRight]}>
+                <Icon style={ticketWalletStyles.bottomNavIcon} name="account-balance-wallet" />
+                <Text style={ticketWalletStyles.bottomNavLinkText}>ADD TO WALLET</Text>
+              </View>
+              <TouchableHighlight>
+                <View style={ticketWalletStyles.bottomNavLinkContainer}>
+                  <Text style={ticketWalletStyles.bottomNavLinkText}>TRANSFER TICKET</Text>
+                  <Icon style={ticketWalletStyles.bottomNavIcon} name="launch" />
+                </View>
+              </TouchableHighlight>
+            </View>
+
           </View>
 
         </View>
