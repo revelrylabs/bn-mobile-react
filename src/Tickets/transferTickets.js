98b85e7246434c0c852b7911c46327797cbfd681
brittanygay <brittany@revelry.co>
2018-10-31 12:02:35 -0500

    added modal container styles

--- src/Tickets/transferTickets.js
+++ src/Tickets/transferTickets.js
@@ -1,14 +1,39 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types'
-import {Text, View} from 'react-native';
+import {Modal, ScrollView, Text, View, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import SharedStyles from '../styles/shared/sharedStyles'
+import TicketWalletStyles from '../styles/tickets/ticketWalletStyles'
+
+const styles = SharedStyles.createStyles()
+const ticketWalletStyles = TicketWalletStyles.createStyles()
+
 export default class TransferTickets extends Component {
 
   render() {
 
     return (
-      <View>
-        <Text>Welcome to the Ticket Xfer Page</Text>
-      </View>
+      <ScrollView>
+        <View style={ticketWalletStyles.modalContainer}>
+          <Image
+            style={ticketWalletStyles.modalBkgdImage}
+            source={require('../../assets/modal-bkgd.jpg')}
+          />
+
+          <View>
+            <View style={ticketWalletStyles.closeModalContainer}>
+              <Icon
+                style={styles.iconLinkCircle}
+                name="close"
+                onPress={() => {
+                  navigation.goBack()
+                }}
+              />
+            </View>
+          </View>
+
+        </View>
+      </ScrollView>
     )
   }
 }
