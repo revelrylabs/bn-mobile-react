1476a0810af66169750ea8e0b01a0a5531a6c538
brittanygay <brittany@revelry.co>
2018-11-05 15:11:44 -0600

    removed un-needed styles

--- src/styles/tickets/ticketTransferStyles.js
+++ src/styles/tickets/ticketTransferStyles.js
@@ -1,24 +1,12 @@
 import {
   white,
   boxShadowColor,
-  sectionHeaderColor,
-  globalFontRegular,
-  bodyFontSize,
-  iconFontSize,
   globalPaddingMedium,
-  globalPadding,
-  globalPaddingSmall,
-  globalPaddingTiny,
   globalMargin,
 } from '../shared/sharedStyles'
 import {StyleSheet, Dimensions, Platform} from 'react-native'
 const fullHeight = Dimensions.get('window').height
 const fullWidth = Dimensions.get('window').width
-
-export const whiteTransparent = 'rgba(255, 255, 255, 0.8)'
-
-export const headerFontSize = 28
-export const bodyFontSizeSmall = 13
 
 const TicketTransferStyles = {
   // CONTAINER STYLES
@@ -33,32 +21,6 @@ const TicketTransferStyles = {
     shadowOpacity: 0.7,
     shadowRadius: 3,
     width: fullWidth - 90,
-  },
-  detailsContainer: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    height: 145,
-  },
-
-  // TEXT STYLES
-  header: {
-    fontFamily: globalFontRegular,
-    fontSize: headerFontSize,
-    color: white,
-    marginBottom: globalPaddingTiny,
-  },
-  detailsBottomHeader: {
-    color: sectionHeaderColor,
-    fontFamily: globalFontRegular,
-    fontSize: bodyFontSizeSmall,
-    letterSpacing: 0.5,
-  },
-  detailsBottomText: {
-    fontSize: bodyFontSize,
-    paddingTop: globalPaddingTiny,
-  },
-  detailsLast: {
-    textAlign: 'right',
   },
 }
 
