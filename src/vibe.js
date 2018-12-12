cadba8932ce55faa29fcaaf1e6951f6da0b10863
jwietelmann <jwietelmann@gmail.com>
2018-12-12 16:29:04 -0500

    better scanner feedback w/vibration

---
+++ src/vibe.js
@@ -0,0 +1,25 @@
+import {Vibration} from 'react-native'
+
+const VIBE_ON = 500 // iOS ignores this param and always chooses ~500ms, so that's why we did 500ms
+const VIBE_OFF = 100
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function pulse(count) {
+  while(count--) {
+    Vibration.vibrate(VIBE_ON)
+    await sleep(VIBE_ON)
+    Vibration.cancel()
+    await sleep(VIBE_OFF)
+  }
+}
+
+export async function happy() {
+  return await pulse(1)
+}
+
+export async function sad() {
+  return await pulse(3)
+}
