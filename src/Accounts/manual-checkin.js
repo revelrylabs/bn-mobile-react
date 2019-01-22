08eb31da61f830a12eb0523acfac64cde1a5203a
jwietelmann <jwietelmann@gmail.com>
2019-01-22 14:48:10 -0500

    more check-in functionality stubbed out

--- src/Accounts/manual-checkin.js
+++ src/Accounts/manual-checkin.js
@@ -1,25 +1,112 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TouchableHighlight} from 'react-native'
+import {price, usernameLastFirst} from '../string'
 
-function Guest({email}) {
+function BusyState() {
+  return (
+    <Text>Hang on a sec...</Text>
+  )
+}
+
+function GuestTicketCard({guest, onSelect}) {
+  return (
+    <TouchableHighlight onPress={() => onSelect(guest)}>
+      <View>
+        <Text>{usernameLastFirst(guest)}</Text>
+        <Text>{guest.ticket_type}</Text>
+        <Text>{price(guest.price_in_cents)}</Text>
+        <Text>{guest.status}</Text>
+      </View>
+    </TouchableHighlight>
+  )
+}
+
+function EmptyState() {
+  return (
+    <Text>No guests found.</Text>
+  )
+}
+
+function GuestList({guests, onSelect}) {
+  if (guests.length === 0) {
+    return <EmptyState />
+  }
+
+  return guests.map((guest) => (
+    <GuestTicketCard
+      key={guest.id}
+      guest={guest}
+      onSelect={onSelect}
+    />
+  ))
+}
+
+function GuestToCheckIn({guest, onCancel, onCheckIn}) {
   return (
     <View>
-      <Text>{email}</Text>
+      <Text>{usernameLastFirst(guest)}</Text>
+      <Text>{guest.ticket_type}</Text>
+      <Text>{price(guest.price_in_cents)}</Text>
+      <Text>{guest.status}</Text>
+      <TouchableHighlight onPress={() => onCancel(guest)}>
+        <Text>Cancel</Text>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={() => onCheckIn(guest)}>
+        <Text>Check In</Text>
+      </TouchableHighlight>
     </View>
   )
 }
 
 export default class ManualCheckin extends Component {
-  render() {
+  state = {
+    selectedGuest: null
+  }
+
+  selectGuest = (selectedGuest) => {
+    this.setState({selectedGuest})
+  }
+
+  unselectGuest = () => {
+    this.selectGuest(null)
+  }
+
+  checkInGuest = (guest) => {
+    console.log(guest)
+    alert(`TODO: check in ticket ID ${guest.id}`)
+    this.unselectGuest()
+  }
+
+  get innerContent() {
+    const {selectedGuest} = this.state
     const {isFetchingGuests, guests} = this.props
 
+    if (selectedGuest !== null) {
+      return (
+        <GuestToCheckIn
+          guest={selectedGuest}
+          onCancel={this.unselectGuest}
+          onCheckIn={this.checkInGuest}
+        />
+      )
+    }
+
+    if (isFetchingGuests) {
+      return <BusyState />
+    }
+
+    return (
+      <GuestList
+        guests={guests}
+        onSelect={this.selectGuest}
+      />
+    )
+  }
+
+  render() {
     return (
       <View style={{flex: 1}}>
-        {isFetchingGuests && (
-          <Text>Hang on a sec...</Text>
-        ) || guests.map(guest => (
-          <Guest {...guest} key={guest.id} />
-        ))}
+        {this.innerContent}
       </View>
     )
   }
