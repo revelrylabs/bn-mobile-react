042b80ddb0ef2424d51080119184badc3c9a17d4
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-02-26 10:29:36 -0600

    Filter Past Events on Event End Date

--- src/time.js
+++ src/time.js
@@ -3,7 +3,8 @@ import {DateTime} from 'luxon'
 const DEFAULT_TIME_ZONE = 'America/Los_Angeles'
 
 export function eventDateTimes({door_time, event_start, event_end}) {
-  const toDateTime = (iso) => iso ? DateTime.fromRFC2822(iso, {setZone: true}) : null
+  const toDateTime = iso =>
+    iso ? DateTime.fromRFC2822(iso, {setZone: true}) : null
 
   return {
     door_time: toDateTime(door_time),
@@ -12,6 +13,12 @@ export function eventDateTimes({door_time, event_start, event_end}) {
   }
 }
 
-export function eventIsInPast({event_start}) {
-  return DateTime.fromISO(event_start) < DateTime.local()
+export function eventIsInPast(event) {
+  const end_time = event.event_end
+    ? event.event_end
+    : DateTime.fromISO(event.event_start)
+        .plus({hours: 6})
+        .toISO()
+
+  return DateTime.fromISO(end_time) < DateTime.local()
 }
