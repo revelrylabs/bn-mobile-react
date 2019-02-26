618093838db3687eee551dd48a5db936b344f9b5
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-02-26 10:31:23 -0600

    comments

--- src/time.js
+++ src/time.js
@@ -1,6 +1,7 @@
 import {DateTime} from 'luxon'
 
 const DEFAULT_TIME_ZONE = 'America/Los_Angeles'
+const EVENT_LENGTH_IN_HOURS = 6
 
 export function eventDateTimes({door_time, event_start, event_end}) {
   const toDateTime = iso =>
@@ -14,10 +15,12 @@ export function eventDateTimes({door_time, event_start, event_end}) {
 }
 
 export function eventIsInPast(event) {
+  // if we have an event end, use that
+  // otherwise add EVENT_LENGTH_IN_HOURS to start time
   const end_time = event.event_end
     ? event.event_end
     : DateTime.fromISO(event.event_start)
-        .plus({hours: 6})
+        .plus({hours: EVENT_LENGTH_IN_HOURS})
         .toISO()
 
   return DateTime.fromISO(end_time) < DateTime.local()
