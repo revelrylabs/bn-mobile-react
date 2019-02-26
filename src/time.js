10b0f22c013641e445a74110cec3dbc7ff16f50a
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-02-26 10:36:37 -0600

    eslint setup

--- src/time.js
+++ src/time.js
@@ -4,7 +4,7 @@ const DEFAULT_TIME_ZONE = 'America/Los_Angeles'
 const EVENT_LENGTH_IN_HOURS = 6
 
 export function eventDateTimes({door_time, event_start, event_end}) {
-  const toDateTime = iso =>
+  const toDateTime = (iso) =>
     iso ? DateTime.fromRFC2822(iso, {setZone: true}) : null
 
   return {
@@ -17,11 +17,11 @@ export function eventDateTimes({door_time, event_start, event_end}) {
 export function eventIsInPast(event) {
   // if we have an event end, use that
   // otherwise add EVENT_LENGTH_IN_HOURS to start time
-  const end_time = event.event_end
-    ? event.event_end
-    : DateTime.fromISO(event.event_start)
-        .plus({hours: EVENT_LENGTH_IN_HOURS})
-        .toISO()
+  const end_time = event.event_end ?
+    event.event_end :
+    DateTime.fromISO(event.event_start)
+      .plus({hours: EVENT_LENGTH_IN_HOURS})
+      .toISO()
 
   return DateTime.fromISO(end_time) < DateTime.local()
 }
