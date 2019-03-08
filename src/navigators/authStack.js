d71677b89a2bf005cd0ad6fa6e91824fb9d6b278
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-03-08 14:20:33 -0600

    Add NetworkContainer to determine if app is online or offline

--- src/navigators/authStack.js
+++ src/navigators/authStack.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {createStackNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation'
 import LogInScreen from '../Auth/LogIn'
 import SignUpScreen from '../Auth/SignUp'
 import SignUpNext from '../Auth/SignUpNext'
 import IndexScreen from '../Auth/index'
 import {Subscribe} from 'unstated'
 import {AuthContainer} from '../state/authStateProvider'
+import {NetworkContainer} from '../state/networkStateProvider'
 import PasswordReset from '../Auth/PasswordReset'
 
 const AuthStack = createStackNavigator(
@@ -23,12 +24,21 @@ const AuthStack = createStackNavigator(
 )
 
 export default class authStackWithStore extends Component {
-  static router = AuthStack.router;
+  static router = AuthStack.router
   static propTypes = {
     navigation: PropTypes.object.isRequired,
   }
 
   render() {
-    return <Subscribe to={[AuthContainer]}>{(auth) => <AuthStack navigation={this.props.navigation} screenProps={{auth}} />}</Subscribe>
+    return (
+      <Subscribe to={[AuthContainer, NetworkContainer]}>
+        {(auth, network) => (
+          <AuthStack
+            navigation={this.props.navigation}
+            screenProps={{auth, network}}
+          />
+        )}
+      </Subscribe>
+    )
   }
 }
