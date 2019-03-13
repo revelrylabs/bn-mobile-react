98f951d8695157317a8417f0f4da30f88dc4a60c
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-03-13 17:37:13 -0500

    use styles.flex1

--- src/Events/checkout.js
+++ src/Events/checkout.js
@@ -114,7 +114,7 @@ export default class Checkout extends Component {
           {this.promoDetails}
 
           <View style={checkoutStyles.rowContainer}>
-            <View style={[checkoutStyles.row, {flex: 1.0}]}>
+            <View style={[checkoutStyles.row, styles.flex1]}>
               <View>
                 <Text style={[checkoutStyles.ticketHeader, styles.marginBottomTiny]}>Quantity</Text>
                 <Text style={checkoutStyles.ticketSubHeader}>{this.ticketType.name}</Text>
