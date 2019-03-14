f3f3290cdc6f866f11ab620be88748ca900314e1
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-03-14 12:49:24 -0500

    Add event search capabilities

--- src/Events/index.js
+++ src/Events/index.js
@@ -1,16 +1,27 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {ScrollView, Text, View, Image, TextInput, TouchableHighlight, Animated, Platform, RefreshControl, Easing} from 'react-native';
-import {NavigationEvents} from 'react-navigation';
+import {
+  ScrollView,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableHighlight,
+  Animated,
+  Platform,
+  RefreshControl,
+  Easing,
+} from 'react-native'
+import {NavigationEvents} from 'react-navigation'
 import Icon from 'react-native-vector-icons/MaterialIcons'
-import ModalDropdown from 'react-native-modal-dropdown';
+import ModalDropdown from 'react-native-modal-dropdown'
 import SharedStyles from '../styles/shared/sharedStyles'
 import FormStyles from '../styles/shared/formStyles'
 import SlideShowStyles from '../styles/shared/slideshowStyles'
 import NavigationStyles from '../styles/shared/navigationStyles'
 import ModalStyles from '../styles/shared/modalStyles'
 import EventItemView from './event_card'
-import {DateTime} from 'luxon';
+import {DateTime} from 'luxon'
 import TicketStyles from '../styles/tickets/ticketStyles'
 import emptyState from '../../assets/icon-empty-state.png'
 
@@ -21,18 +32,19 @@ const navigationStyles = NavigationStyles.createStyles()
 const modalStyles = ModalStyles.createStyles()
 const ticketStyles = TicketStyles.createStyles()
 
-const HEADER_MAX_HEIGHT = 0;
-const HEADER_MIN_HEIGHT = -25;
-const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+const HEADER_MAX_HEIGHT = 0
+const HEADER_MIN_HEIGHT = -25
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT
 
 function EmptyEvents({locationName}) {
   return (
     <View style={ticketStyles.emptyStateContainer}>
-      <Image
-        style={ticketStyles.emptyStateIcon}
-        source={emptyState}
-      />
-      <Text style={ticketStyles.emptyStateText}>{"More" + (locationName == "All Locations" ? null : " " + locationName) + " events and experiences powered by Big Neon launching soon!"}</Text>
+      <Image style={ticketStyles.emptyStateIcon} source={emptyState} />
+      <Text style={ticketStyles.emptyStateText}>
+        {`More${
+          locationName == 'All Locations' ? '' : ` ${locationName}`
+        } events and experiences powered by Big Neon launching soon!`}
+      </Text>
     </View>
   )
 }
@@ -45,13 +57,15 @@ export default class EventsIndex extends Component {
 
   /* eslint-disable-next-line complexity */
   constructor(props) {
-    super(props);
-    const {screenProps: {store}} = props
+    super(props)
+    const {
+      screenProps: {store},
+    } = props
     const {state} = store
 
     this.state = {
       scrollY: new Animated.Value(
-        Platform.OS === 'ios' ? -HEADER_MAX_HEIGHT : 0,
+        Platform.OS === 'ios' ? -HEADER_MAX_HEIGHT : 0
       ),
       refreshing: false,
       toValue: 1,
@@ -59,12 +73,19 @@ export default class EventsIndex extends Component {
       easing: Easing.linear,
       selectedLocationId: state.selectedLocationId || 2,
       mainFavorite: true,
-    };
+      searchText: '',
+    }
   }
 
   componentWillReceiveProps(newProps) {
     // Check for updated Location
-    const {screenProps: {store: {state: {selectedLocationId}}}} = newProps
+    const {
+      screenProps: {
+        store: {
+          state: {selectedLocationId},
+        },
+      },
+    } = newProps
 
     if (selectedLocationId !== this.state.selectedLocationId) {
       // also do some kind of event re-search action to load new city events
@@ -74,43 +95,80 @@ export default class EventsIndex extends Component {
 
   get locations() {
     return [
-      {id: null, name: 'Where are you looking for events?', selectedName: 'All Locations'},
+      {
+        id: null,
+        name: 'Where are you looking for events?',
+        selectedName: 'All Locations',
+      },
       ...this.props.screenProps.store.state.locations,
     ]
   }
 
   loadEvents() {
-    const {screenProps: {store}} = this.props
+    const {
+      screenProps: {store},
+    } = this.props
 
     if (this.events.length === 0 || this.eventsRefresh) {
       store.getEvents()
+      this.setState({searchText: ''})
     }
   }
 
   get eventsRefresh() {
-    const {screenProps: {store: {state: {lastUpdate}}}} = this.props
+    const {
+      screenProps: {
+        store: {
+          state: {lastUpdate},
+        },
+      },
+    } = this.props
 
     return !lastUpdate || lastUpdate.plus({minutes: 15}) < DateTime.local()
   }
 
   get events() {
-    const {screenProps: {store: {state: {events, selectedLocationId}}}} = this.props
+    const {
+      screenProps: {
+        store: {
+          state: {events, selectedLocationId},
+        },
+      },
+    } = this.props
 
-    if (!selectedLocationId) {
-      return events
+    let eventsToDisplay = events
+
+    if (selectedLocationId) {
+      eventsToDisplay = eventsToDisplay.filter(
+        ({venue: {region_id}}) => region_id === selectedLocationId
+      )
     }
 
-    return events.filter(({venue: {region_id}}) => region_id === selectedLocationId)
+    if (this.state.searchText !== '') {
+      eventsToDisplay = eventsToDisplay.filter(({name}) =>
+        name.toLowerCase().startsWith(this.state.searchText.toLowerCase())
+      )
+    }
+
+    return eventsToDisplay
   }
 
   setFavorite = (mainFavorite) => {
     this.setState({mainFavorite})
   }
 
-  get currentLocationDisplayName() {
-    const selectedLoc = this.locations.find((loc) => (loc.id === this.state.selectedLocationId))
+  updateSearchText = (text) => {
+    this.setState({
+      searchText: text,
+    })
+  }
 
-    return selectedLoc && (selectedLoc.selectedName || selectedLoc.name) || ''
+  get currentLocationDisplayName() {
+    const selectedLoc = this.locations.find(
+      (loc) => loc.id === this.state.selectedLocationId
+    )
+
+    return (selectedLoc && (selectedLoc.selectedName || selectedLoc.name)) || ''
   }
 
   locRowOption = (rowData, rowID, _highlighted) => {
@@ -135,10 +193,15 @@ export default class EventsIndex extends Component {
   }
 
   get allEvents() {
-    const {navigation: {navigate}, screenProps: {store: {toggleInterest}}} = this.props
+    const {
+      navigation: {navigate},
+      screenProps: {
+        store: {toggleInterest},
+      },
+    } = this.props
     const events = this.events
 
-    if(events.length == 0){
+    if (events.length === 0) {
       return <EmptyEvents locationName={this.currentLocationDisplayName} />
     }
     return events.map((event, index) => (
@@ -151,48 +214,53 @@ export default class EventsIndex extends Component {
     ))
   }
 
+  searchEvents = (query) => {
+    this.props.searchEvents(query)
+  }
+
   /* eslint-disable-next-line complexity */
   render() {
     const scrollY = Animated.add(
       this.state.scrollY,
-      Platform.OS === 'ios' ? HEADER_MAX_HEIGHT : 0,
-    );
+      Platform.OS === 'ios' ? HEADER_MAX_HEIGHT : 0
+    )
     const headerHeight = this.state.scrollY.interpolate({
       inputRange: [0, HEADER_SCROLL_DISTANCE],
       outputRange: [HEADER_MAX_HEIGHT, 100],
       extrapolate: 'clamp',
-    });
+    })
     const headerTranslate = scrollY.interpolate({
       inputRange: [0, 100],
       outputRange: [0, -HEADER_SCROLL_DISTANCE],
       extrapolate: 'clamp',
-    });
+    })
     const opacity = scrollY.interpolate({
       inputRange: [0, 0.9, 1],
       outputRange: [1, 0, 1],
-    });
+    })
 
-    const {navigation: {navigate}, screenProps: {store}} = this.props
+    const {
+      navigation: {navigate},
+      screenProps: {store},
+    } = this.props
     const {mainFavorite} = this.state
 
     return (
       <View style={styles.containerFullHeight}>
-        <NavigationEvents
-          onWillFocus={() => this.loadEvents()}
-        />
+        <NavigationEvents onWillFocus={() => this.loadEvents()} />
         <ScrollView
-          style={{flex:1}}
+          style={{flex: 1}}
           scrollEventThrottle={16}
           showsVerticalScrollIndicator={false}
-          onScroll={Animated.event(
-            [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
-          )}
+          onScroll={Animated.event([
+            {nativeEvent: {contentOffset: {y: this.state.scrollY}}},
+          ])}
           refreshControl={
             <RefreshControl
               refreshing={this.state.refreshing}
               onRefresh={() => {
-                this.setState({refreshing: true});
-                setTimeout(() => this.setState({refreshing: false}), 1000);
+                this.setState({refreshing: true})
+                setTimeout(() => this.setState({refreshing: false}), 1000)
               }}
               // Android offset for RefreshControl
               progressViewOffset={HEADER_MAX_HEIGHT}
@@ -206,8 +274,12 @@ export default class EventsIndex extends Component {
             y: -HEADER_MAX_HEIGHT,
           }}
         >
-          <View style={[styles.sectionHeaderContainer, styles.flexRowSpaceBetween]}>
-            <Animated.Text style={[styles.header, {opacity}]}>Explore</Animated.Text>
+          <View
+            style={[styles.sectionHeaderContainer, styles.flexRowSpaceBetween]}
+          >
+            <Animated.Text style={[styles.header, {opacity}]}>
+              Explore
+            </Animated.Text>
             <ModalDropdown
               ref={(ref) => {
                 this._dropdown = ref
@@ -223,13 +295,14 @@ export default class EventsIndex extends Component {
                   style={styles.iconImageSmall}
                   source={require('../../assets/heart-small.png')}
                 />
-                <Text style={styles.iconLinkText}>{this.currentLocationDisplayName}</Text>
+                <Text style={styles.iconLinkText}>
+                  {this.currentLocationDisplayName}
+                </Text>
                 <Icon style={styles.iconLink} name="keyboard-arrow-down" />
               </View>
             </ModalDropdown>
           </View>
 
-          {false && // TODO: Re-enable when functionality is implemented.
           <View style={formStyles.searchContainer}>
             <Image
               style={formStyles.searchIcon}
@@ -240,64 +313,103 @@ export default class EventsIndex extends Component {
               placeholder="Search artists, shows, venues..."
               searchIcon={{size: 24}}
               underlineColorAndroid="transparent"
+              onChangeText={this.updateSearchText}
               disabled
             />
           </View>
-          }
 
-          {false && // TODO: Re-enable when functionality is implemented.
-          <Text style={styles.sectionHeader}>Hot This Week</Text>
-          }
+          {this.state.searchText !== '' && (
+            <Text style={styles.sectionHeader}>
+              {`Search Results for "${this.state.searchText}"`}
+            </Text>
+          )}
 
-          {false && // TODO: Re-enable when functionality is implemented.
-          <TouchableHighlight underlayColor="rgba(0, 0, 0, 0)" onPress={() => navigate('EventsShow', {name: 'Childish Gambino'})}>
-            <View style={slideshowStyles.slideshowContainer}>
-              <Image
-                style={slideshowStyles.slideShowImage}
-                source={require('../../assets/featured-1.png')}
-              />
-              <Image
-                style={slideshowStyles.slideShowImage}
-                source={require('../../assets/featured-img-overlay.png')}
-              />
+          {false && (
+            <Text
+              style={
+                styles.sectionHeader // TODO: Re-enable when functionality is implemented.
+              }
+            >
+              Hot This Week
+            </Text>
+          )}
 
-              <View style={slideshowStyles.detailsContainer}>
-                <View style={slideshowStyles.sectionTop}>
-                  <TouchableHighlight underlayColor="rgba(0, 0, 0, 0)" onPress={() => this.setFavorite(!mainFavorite)}>
-                    <View style={mainFavorite ? styles.iconLinkCircleContainerActive : styles.iconLinkCircleContainer}>
-                      <Icon style={mainFavorite ? styles.iconLinkCircleActive : styles.iconLinkCircle} name="star" />
+          {false && ( // TODO: Re-enable when functionality is implemented.
+            <TouchableHighlight
+              underlayColor="rgba(0, 0, 0, 0)"
+              onPress={() => navigate('EventsShow', {name: 'Childish Gambino'})}
+            >
+              <View style={slideshowStyles.slideshowContainer}>
+                <Image
+                  style={slideshowStyles.slideShowImage}
+                  source={require('../../assets/featured-1.png')}
+                />
+                <Image
+                  style={slideshowStyles.slideShowImage}
+                  source={require('../../assets/featured-img-overlay.png')}
+                />
+
+                <View style={slideshowStyles.detailsContainer}>
+                  <View style={slideshowStyles.sectionTop}>
+                    <TouchableHighlight
+                      underlayColor="rgba(0, 0, 0, 0)"
+                      onPress={() => this.setFavorite(!mainFavorite)}
+                    >
+                      <View
+                        style={
+                          mainFavorite ?
+                            styles.iconLinkCircleContainerActive :
+                            styles.iconLinkCircleContainer
+                        }
+                      >
+                        <Icon
+                          style={
+                            mainFavorite ?
+                              styles.iconLinkCircleActive :
+                              styles.iconLinkCircle
+                          }
+                          name="star"
+                        />
+                      </View>
+                    </TouchableHighlight>
+                    <View style={styles.avatarContainer}>
+                      <Image
+                        style={styles.avatar}
+                        source={require('../../assets/avatar-male.png')}
+                      />
+                      <Image
+                        style={styles.avatar}
+                        source={require('../../assets/avatar-female.png')}
+                      />
                     </View>
-                  </TouchableHighlight>
-                  <View style={styles.avatarContainer}>
-                    <Image
-                      style={styles.avatar}
-                      source={require('../../assets/avatar-male.png')}
-                    />
-                    <Image
-                      style={styles.avatar}
-                      source={require('../../assets/avatar-female.png')}
-                    />
                   </View>
-                </View>
 
-                <View style={slideshowStyles.sectionMiddle}>
-                  <Icon style={slideshowStyles.slideShowIconLinkLeft} name="keyboard-arrow-left" />
-                  <Icon style={slideshowStyles.slideShowIconLinkRight} name="keyboard-arrow-right" />
-                </View>
-                <View>
-                  <View style={styles.priceTagContainer}>
-                    <Text style={styles.priceTag}>$30</Text>
+                  <View style={slideshowStyles.sectionMiddle}>
+                    <Icon
+                      style={slideshowStyles.slideShowIconLinkLeft}
+                      name="keyboard-arrow-left"
+                    />
+                    <Icon
+                      style={slideshowStyles.slideShowIconLinkRight}
+                      name="keyboard-arrow-right"
+                    />
                   </View>
-                  <Text style={slideshowStyles.header}>Childish Gambino</Text>
-                  <View style={styles.flexRowSpaceBetween}>
-                    <Text style={slideshowStyles.details}>Fox Theater  &bull;  Oakland, CA</Text>
-                    <Text style={slideshowStyles.details}>July 15, 2018</Text>
+                  <View>
+                    <View style={styles.priceTagContainer}>
+                      <Text style={styles.priceTag}>$30</Text>
+                    </View>
+                    <Text style={slideshowStyles.header}>Childish Gambino</Text>
+                    <View style={styles.flexRowSpaceBetween}>
+                      <Text style={slideshowStyles.details}>
+                        Fox Theater &bull; Oakland, CA
+                      </Text>
+                      <Text style={slideshowStyles.details}>July 15, 2018</Text>
+                    </View>
                   </View>
                 </View>
               </View>
-            </View>
-          </TouchableHighlight>
-          }
+            </TouchableHighlight>
+          )}
 
           <View style={styles.spacer} />
 
@@ -305,13 +417,22 @@ export default class EventsIndex extends Component {
 
           <View style={styles.spacer} />
         </ScrollView>
-        <Animated.View style={[navigationStyles.scrollHeaderContainer, {height: headerHeight, transform: [{translateY: headerTranslate}]}]}>
+        <Animated.View
+          style={[
+            navigationStyles.scrollHeaderContainer,
+            {height: headerHeight, transform: [{translateY: headerTranslate}]},
+          ]}
+        >
           <View style={navigationStyles.scrollHeader}>
-            <Animated.Text style={[navigationStyles.scrollTitle, {opacity}]}>Explore</Animated.Text>
-            <Animated.Text style={navigationStyles.scrollSubTitle}>All Dates &bull; {this.currentLocationDisplayName}</Animated.Text>
+            <Animated.Text style={[navigationStyles.scrollTitle, {opacity}]}>
+              Explore
+            </Animated.Text>
+            <Animated.Text style={navigationStyles.scrollSubTitle}>
+              All Dates &bull; {this.currentLocationDisplayName}
+            </Animated.Text>
           </View>
         </Animated.View>
       </View>
-    );
+    )
   }
 }
