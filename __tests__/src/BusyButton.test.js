753973896d9b0241474c0c130039966d937811f4
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-04-18 14:05:06 -0500

    Use safer test functions

--- __tests__/src/BusyButton.test.js
+++ __tests__/src/BusyButton.test.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import {Text, TouchableHighlight} from 'react-native'
+import {Text} from 'react-native'
 import {render} from 'react-native-testing-library'
 import BusyButton from '../../src/BusyButton'
 
 it('renders', () => {
-  const {queryByType, queryByText} = render(
+  const {queryByText} = render(
     <BusyButton>
       <Text>Hi</Text>
     </BusyButton>
   )
 
-  expect(queryByType(TouchableHighlight)).not.toBeNull()
   expect(queryByText('Hi')).not.toBeNull()
   expect(queryByText('Loading')).toBeNull()
 })
