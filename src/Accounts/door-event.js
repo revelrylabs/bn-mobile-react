6178bcaeedfd8604a588df0d2878f1840fb9565c
J. Patrick <jpatricknola@gmail.com>
2019-05-22 19:12:51 -0500

    access guest list from event for doorperson

--- src/Accounts/door-event.js
+++ src/Accounts/door-event.js
@@ -41,7 +41,7 @@ function DoorEventDashboard({
   event: {tickets_redeemed: redeemed, sold_held: sold1, sold_unreserved: sold2},
 }) {
   return (
-    <View>
+    <View style={eventManagerStyles.redeemedStats}>
       <HeaderText>
         {redeemed} of {sold1 + sold2} redeemed
       </HeaderText>
@@ -49,23 +49,18 @@ function DoorEventDashboard({
   )
 }
 
-function DoorEventSummary({event: {name}, dashboard, onPressScan}) {
+function DoorEventSummary({event: {name}, dashboard, onPressScan, onPressGuestList}) {
   return (
     <View>
       <HeaderText>{name}</HeaderText>
       <ScanButton onPress={onPressScan} />
+      <GuestList onPress={onPressGuestList} />
       {dashboard && <DoorEventDashboard {...dashboard} />}
     </View>
   )
 }
 
-function GuestList() {
-  // await this.props.screenProps.eventManager.scanForEvent(this.event)
-  this.props.navigation.navigate('GuestList')
-}
-
-
-function GuestListTab({onPress}) {
+function GuestList({onPress}) {
   return (
     <View style={styles.buttonContainer}>
       <TouchableHighlight style={styles.button} onPress={onPress}>
@@ -74,6 +69,7 @@ function GuestListTab({onPress}) {
     </View>
   )
 }
+
 
 export default class DoorEvent extends Component {
   state = {
@@ -110,6 +106,11 @@ export default class DoorEvent extends Component {
     this.props.navigation.navigate('EventScanner')
   }
 
+  guestListButton = async() =>{
+    await this.props.screenProps.eventManager.scanForEvent(this.event)
+    this.props.navigation.navigate('GuestList')
+  }
+
   render() {
     return (
       <View style={styles.containerFullHeight}>
@@ -118,6 +119,7 @@ export default class DoorEvent extends Component {
           event={this.event}
           dashboard={this.dashboardData}
           onPressScan={this.chooseEvent}
+          onPressGuestList={this.guestListButton}
         />
       </View>
     )
